fd8ac03eb0d1fa4f23d8b76ecf07e79886a39311
Muhammad Umair <umaair40@gmail.com>
2025-09-21 23:39:18 +0500

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,5 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+// ⚡ Remove app.listen for Vercel
 module.exports = app;
